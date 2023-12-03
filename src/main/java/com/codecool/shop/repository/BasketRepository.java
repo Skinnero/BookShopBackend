b86e12525fe3bc83ddba94863e4a8d3a4ec635d1
Kacper Tomczyk <46459137+Skinnero@users.noreply.github.com>
2023-12-03 17:31:28 +0100

    Removing unnecessery queries and fixing old one to resolve n+1 problem and lazy loading

--- src/main/java/com/codecool/shop/repository/BasketRepository.java
+++ src/main/java/com/codecool/shop/repository/BasketRepository.java
@@ -1,7 +1,6 @@
 package com.codecool.shop.repository;
 
 import com.codecool.shop.repository.entity.Basket;
-import com.codecool.shop.repository.entity.projection.BasketProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -12,24 +11,14 @@ import java.util.UUID;
 
 @Repository
 public interface BasketRepository extends JpaRepository<Basket, UUID> {
-    @Query("select b from Basket b where b.id = :id")
+    @Query("select b from Basket b left join fetch b.products where b.id = :id")
     Optional<Basket> findById(UUID id);
 
-    @Query(nativeQuery = true, value = """
-            select id as basketId, p.product_id as productId, count(p.product_id) as quantity from basket
-            left join basket_product as p on id = p.basket_id
-            where id = :id and is_complete = false
-            group by id, p.product_id
-            """)
-    List<BasketProjection> findProductsInBasketById(UUID id);
+    @Query("select b from Basket b left join fetch b.products where b.customer.id = :customerId and b.isComplete = false")
+    Optional<Basket> findByCustomerId(UUID customerId);
 
-    @Query(nativeQuery = true, value = """
-            select id as basketId, p.product_id as productId, count(p.product_id) as quantity from basket
-            left join basket_product as p on id = p.basket_id
-            where basket.customer_id = :id and is_complete = false
-            group by id, p.product_id
-            """)
-    List<BasketProjection> findProductsInBasketByCustomerId(UUID id);
+    @Query("select b from Basket b left join fetch b.products where b.customer.id = :customerId")
+    List<Basket> findAllByCustomerId(UUID customerId);
 
     void deleteById(UUID id);
 }
